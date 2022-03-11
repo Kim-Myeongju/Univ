a0b25ab234297e2445d7f7026a594a728364faf5
ony36400 <ony36400@naver.com>
2022-03-11 14:05:40 +0900

    commit

---
+++ draw_triangle.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle() #turtle.Pen()=turtle.Turtle()
+
+'''
+t.shape("turtle")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+'''
+
+for i in [0,1,2]:
+    t.forward(100)
+    t.left(120)
