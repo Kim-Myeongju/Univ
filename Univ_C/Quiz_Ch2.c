a4244262a36bcffa982de123c478aba3eeddbca5
DDAKJII <ony36400@naver.com>
2022-03-17 04:09:41 +0900

    cc

--- Univ_C/Quiz_Ch2.c
+++ Univ_C/Quiz_Ch2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main(void)
-{
+int main(void) {
     printf("Hello\nI am a student at Hongik University.\nMy age is %d and My ID is %s.", 20, "C211026");
     /*
     정수 20은 정수형 서식문자인 %d를 통해 사용할 수 있지만, 학번에는 문자와 숫자가 함께 있으므로 %d를 사용할 수 없다.
