769f2b97553f80cf77ce1d0f35fd1fb60e2d563e
ony36400 <ony36400@naver.com>
2022-03-16 15:07:45 +0900

    commit

--- Univ_C/Quiz_Ch2.c
+++ Univ_C/Quiz_Ch2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("Hello\nI am a student at Hongik University.\nMy age is %d and My ID is %s", 20, "C211026");
+    printf("Hello\nI am a student at Hongik University.\nMy age is %d and My ID is %s", 20, "C211026"); //%d에 정수 20, %s에 문자열 "C211026"이 출력됨
     return 0;
 }
